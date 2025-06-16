14290d9bd29e9be83df33116ccc3212f1b976022
zakiia <zakiasultanabhuiyan@gmail.com>
2025-06-17 05:30:20 +0600

    routing and navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
 import { Text, View } from "react-native";
 
 export default function Index() {
+  // @ts-ignore
+  // @ts-ignore
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className=" text-5xl text-blue-500 font-bold">
-        Tailwind not working
+      <Text className=" text-5xl text-dark-200 font-bold">
+        Tailwind working!
       </Text>
     </View>
   );
