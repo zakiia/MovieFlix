80d51ae3f64b7ddc402ef597e6cf526098eecf85
zakiia <zakiasultanabhuiyan@gmail.com>
2025-06-17 06:16:01 +0600

    home screen ui

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,30 @@
-import { Text, View } from "react-native";
+import SearchBar from "@/component/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { useRouter } from "expo-router";
+import { Image, ScrollView, View } from "react-native";
 
 export default function Index() {
+  const router = useRouter();
   // @ts-ignore
   // @ts-ignore
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className=" text-5xl text-dark-200 font-bold">
-        Tailwind working!
-      </Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0" />
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+      >
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+
+        <View className="flex-1 mt-5">
+          <SearchBar
+            onPress={() => router.push("/search")}
+            placeholder="Search for a movie"
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 }
